9d5b2e3a0a36197684cb9ed0043dfea16ed0fd9d
edwardPew <edik.tv@mail.ru>
2021-04-23 17:26:42 +0600

    fixed left window

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const webSocket=require('ws')
 const server=new webSocket.Server({port:1000})
 
+let settingsObject={chanels:[true,false,true,false,true,false],sQual:50,tRTC:30}
+
 server.on('connection',(ws)=>{
-    ws.send('wellcome')
+    ws.send(JSON.stringify(settingsObject));
+    ws.on('message',(messageData)=>{
+        server.clients.forEach(client=>{
+            settingsObject={...settingsObject,...JSON.parse(messageData)}
+            if(client.readyState===webSocket.OPEN){
+                    ws.send(JSON.stringify(settingsObject))
+            }
+        })
+    })
 })
