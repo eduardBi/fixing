dd5f5fc81b080ae89a8b1caaf53e109f770b2d79
edwardPew <edik.tv@mail.ru>
2021-04-24 00:18:33 +0600

    handled all windows

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const webSocket=require('ws')
 const server=new webSocket.Server({port:1000})
 
-let settingsObject={chanels:[true,false,true,false,true,false],sQual:50,tRTC:30}
+let settingsObject={chanels:[{isOpen:false,temperature:50},{isOpen:true,temperature:70},{isOpen:true,temperature:20},{isOpen:true,temperature:50},{isOpen:true,temperature:70},{isOpen:true,temperature:70}],sQual:50,tRTC:30}
 
 server.on('connection',(ws)=>{
     ws.send(JSON.stringify(settingsObject));
